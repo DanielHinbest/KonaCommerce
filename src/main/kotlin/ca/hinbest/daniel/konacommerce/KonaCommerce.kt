7a86ef2c035877b4e51812e99768c2c775d814b1
Daniel Hinbest <60987457+DanielHinbest@users.noreply.github.com>
2024-11-06 11:21:42 -0500

    Fixes some problematic issues with the runtime

--- src/main/kotlin/ca/hinbest/daniel/konacommerce/KonaCommerce.kt
+++ src/main/kotlin/ca/hinbest/daniel/konacommerce/KonaCommerce.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 // TODO: Finish code for models and repositories, when this is done, build the services and create unit tests for them all
 @SpringBootApplication
-class KonaCommerceApplication
+class KonaCommerce
 
 fun main(args: Array<String>) {
-    runApplication<KonaCommerceApplication>(*args)
+    runApplication<KonaCommerce>(*args)
 }
